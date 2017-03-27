07996762840e7211814c4b8001e456bf1a36f3fe
R. Anthony Porter <rah@sheenonline.biz>
2017-03-26 21:53:05 -0400

    Added transpiling with babel, basic app, webpack config, and eslint-watch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,15 +1,25 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+/* eslint-disable no-console */
 
-app.get('/', function (req, res) {
+const port = process.env.PORT || 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function (err) {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
